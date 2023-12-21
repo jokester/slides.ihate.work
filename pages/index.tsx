6ec03fe750df21f75049d18b78f1a667f931be39
Wang Guan <momocraft@gmail.com>
2023-12-22 01:42:56 +0900

    build for cf page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
+export const runtime = 'experimental-edge';
+
 const IndexPage: NextPage = (props) => {
   const router = useRouter();
   useEffect(() => {
--- pages/markdown.tsx
+++ pages/markdown.tsx
@@ -1,3 +1,5 @@
 import { MarkdownPage } from '../src/pages/markdown';
 
+export const runtime = 'experimental-edge';
+
 export default MarkdownPage;
