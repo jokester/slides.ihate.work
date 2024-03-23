1500787f4f18d9a782d3e09d82687bb667d9f15b
Wang Guan <momocraft@gmail.com>
2024-03-24 00:18:24 +0900

    preload reveal player in all pages

---
+++ src/player/use-reveal-preload.ts
@@ -0,0 +1,11 @@
+import { useAsyncEffect } from '@jokester/ts-commonutil/lib/react/hook/use-async-effect';
+
+export function useRevealPreload() {
+  useAsyncEffect(
+    async () => {
+      await import('./reveal-init');
+    },
+    [],
+    true,
+  );
+}
